405aa19c1e76d2d8dd4692a523273f80b84ffd40
GaDget-TyP3-0perator <140066280+GaDget-TyP3-0perator@users.noreply.github.com>
2023-07-21 09:13:50 +1000

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+# ###  In this notebook, Data Science Tools and Ecosystem are sumarised 
+# 
+
+# ***Some of the popular languages that Data Scientists use***
+# 
+# - Python 
+# - R
+# - SQL
+# - Java
+# - Scala
+# - C++
+# - Julia
+# 
+
+# ***Some commonly used libraries used by Data Scientists include:***
+#     
+#     1. Pandas
+#     2. Numpy
+#     3. MatplotLib
+#     4. Seaborn
+#     5. Scikit-learn
+#     6. Ggplot
+#     
+#     
+
+# | Data Science Tools | 
+# |--------------------|
+# | Jupyter Lab|
+# | R Studio |
+# | Apache Zeppeelin |
+# | Spyder |
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expression s in Python
+# 
+
+# In[2]:
+
+
+##This is a simple arithmetic expression to multiply then add integers.
+
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[11]:
+
+
+#This will convernt users inputs "Minutes" into hours.
+Min = int(input("Enter Minutes:"))
+Hours = Min / 60
+
+print(Hours)
+
+
+# **Objectives**
+# - Markdown cells
+#     - Headings
+#     - Tables
+#     - Italics and bold fonts 
+# - Data science tools
+#     - Environments
+#     - Languages 
+#     - Libraries
+
+# ## Author
+# James 
+
+# In[ ]:
+
+
+
+
